e9138af8ee4170651428c9b1ec1b579348253f66
zhuohl <109897686+zhuohl@users.noreply.github.com>
2022-07-26 01:43:02 +0800

    Update cs3.php

--- cs3.php
+++ cs3.php
@@ -1,4 +1,8 @@
 <?php
 $cars=array("porsche","BMW","Volvo"); 
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+$cars[0]="porsche";
+$cars[1]="BMW";
+$cars[2]="Volvo";
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 ?>
